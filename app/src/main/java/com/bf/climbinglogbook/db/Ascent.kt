e99c04b70f0b60a5ebf13c3ea532ea640cd4ad86
bertonoon <filipow.bartosz@gmail.com>
2023-07-01 11:12:05 +0200

    refactor ascent

--- app/src/main/java/com/bf/climbinglogbook/db/Ascent.kt
+++ app/src/main/java/com/bf/climbinglogbook/db/Ascent.kt
@@ -7,27 +7,32 @@ import com.bf.climbinglogbook.models.BelayType
 import com.bf.climbinglogbook.models.ClimbingType
 import com.bf.climbinglogbook.models.AscentStyle
 import com.bf.climbinglogbook.models.GradeSystem
+import com.bf.climbinglogbook.models.gradeEnums.Grade
 import com.bf.climbinglogbook.other.Constants.ROUTES_TABLE_NAME
 
 @Entity(tableName = ROUTES_TABLE_NAME)
 data class Ascent (
     var name : String,
     var grade : Int = 0,
+    var gradeSystem : GradeSystem,
+    var originalGrade : String,
+    var hard : Boolean = false,
+
+    var comment : String? = null,
+
     var country : String? = null,
     var region : String? = null,
     var rock : String? = null,
     var lat : Long = 0L,
     var lng : Long = 0L,
-    var comment : String? = null,
+
     var img : Bitmap? = null,
     var meters : Int = 0,
-    var gradeType : GradeSystem = GradeSystem.FRENCH,
     var belayType : BelayType? = null,
     var climbingType : ClimbingType? = null,
     var belayer : String? = null,
     var ascentStyle : AscentStyle,
-    var pitches : Int = 1,
-    var hard : Boolean = false
+    var pitches : Int = 1
     ){
     @PrimaryKey(autoGenerate = true)
     var id: Int? = null
