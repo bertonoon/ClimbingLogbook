1cf3bcbcdeed1e781fa6339ccd25f48999a72447
bart <filipow.bartosz@gmail.com>
2023-12-06 16:46:09 +0100

    add tests to ascentDaoTest

--- app/src/androidTest/java/com/bf/climbinglogbook/db/AscentDAOTest.kt
+++ app/src/androidTest/java/com/bf/climbinglogbook/db/AscentDAOTest.kt
@@ -6,6 +6,8 @@ import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import com.bf.climbinglogbook.getOrAwaitValue
+import com.bf.climbinglogbook.models.AscentStyle
+import com.bf.climbinglogbook.models.ClimbingType
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runTest
 import org.junit.After
@@ -14,6 +16,8 @@ import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import org.mockito.internal.stubbing.answers.ThrowsException
+import java.lang.AssertionError
 
 @ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
@@ -73,7 +77,134 @@ class AscentDAOTest {
         insertAscents()
         val ascentsFromDb = dao.getAllRoutesSortedByNameAsc().getOrAwaitValue()
 
-        val ascentsSortedByName = ascents.sortedBy { it.name }
-        assertEquals(ascentsSortedByName, ascentsFromDb)
+        val ascentsSorted = ascents.sortedBy { it.name }
+        assertEquals(ascentsSorted, ascentsFromDb)
     }
+    @Test
+    fun getAllRoutesSortedByNameDesc() = runTest {
+        insertAscents()
+        val ascentsFromDb = dao.getAllRoutesSortedByNameDesc().getOrAwaitValue()
+
+        val ascentsSorted = ascents.sortedByDescending { it.name }
+        assertEquals(ascentsSorted, ascentsFromDb)
+    }
+    @Test
+    fun getAllRoutesSortedByDateAsc() = runTest {
+        insertAscents()
+        val ascentsFromDb = dao.getAllRoutesSortedByDateAsc().getOrAwaitValue()
+
+        val ascentsSorted = ascents.sortedBy { it.date }
+        assertEquals(ascentsSorted, ascentsFromDb)
+    }
+    @Test
+    fun getAllRoutesSortedByDateDesc() = runTest {
+        insertAscents()
+        val ascentsFromDb = dao.getAllRoutesSortedByDateDesc().getOrAwaitValue()
+
+        val ascentsSorted = ascents.sortedByDescending { it.date }
+        assertEquals(ascentsSorted, ascentsFromDb)
+    }
+    @Test
+    fun getAllRoutesSortedByAscentStyleAsc() = runTest {
+        insertAscents()
+        val ascentsFromDb = dao.getAllRoutesSortedByAscentStyleAsc().getOrAwaitValue()
+
+        val ascentsSorted = ascents.sortedBy { it.ascentStyle.name }
+        assertEquals(ascentsSorted, ascentsFromDb)
+    }
+    @Test
+    fun getAllRoutesSortedByAscentStyleDesc() = runTest {
+        insertAscents()
+        val ascentsFromDb = dao.getAllRoutesSortedByAscentStyleDesc().getOrAwaitValue()
+
+        val ascentsSorted = ascents.sortedByDescending { it.ascentStyle.name }
+        assertEquals(ascentsSorted, ascentsFromDb)
+    }
+    @Test
+    fun getAllRoutesSortedByMetersAsc() = runTest {
+        insertAscents()
+        val ascentsFromDb = dao.getAllRoutesSortedByMetersAsc().getOrAwaitValue()
+
+        val ascentsSorted = ascents.sortedBy { it.meters }
+        assertEquals(ascentsSorted, ascentsFromDb)
+    }
+    @Test
+    fun getAllRoutesSortedByMetersDesc() = runTest {
+        insertAscents()
+        val ascentsFromDb = dao.getAllRoutesSortedByMetersDesc().getOrAwaitValue()
+
+        val ascentsSorted = ascents.sortedByDescending { it.meters }
+        assertEquals(ascentsSorted, ascentsFromDb)
+    }
+    @Test
+    fun getAllRoutesSortedByGradeAsc() = runTest {
+        insertAscents()
+        val ascentsFromDb = dao.getAllRoutesSortedByGradeAsc().getOrAwaitValue()
+
+        val ascentsSorted = ascents.sortedBy { it.usaGradeNumber }
+        assertEquals(ascentsSorted, ascentsFromDb)
+    }
+    @Test
+    fun getAllRoutesSortedByGradeDesc() = runTest {
+        insertAscents()
+        val ascentsFromDb = dao.getAllRoutesSortedByGradeDesc().getOrAwaitValue()
+
+        val ascentsSorted = ascents.sortedByDescending { it.usaGradeNumber }
+        assertEquals(ascentsSorted, ascentsFromDb)
+    }
+
+    @Test
+    fun numberOfItemsInDb() = runTest {
+        insertAscents()
+        val numberOfItemsInDb = dao.numberOfItemsInDB().getOrAwaitValue()
+        assertEquals(numberOfItemsInDb,ascents.size)
+    }
+
+    @Test
+    fun numberOfAscentsByStyle() = runTest{
+        insertAscents()
+        val numberOfItemsInDb = dao.numberOfOsAscentsByStyle(AscentStyle.REDPOINT).getOrAwaitValue()
+        val numOfRedpointAscents = ascents.count { it.ascentStyle == AscentStyle.REDPOINT }
+        assertEquals(numberOfItemsInDb,numOfRedpointAscents)
+    }
+
+    @Test
+    fun numberOfAscentsByClimbingType() = runTest{
+        insertAscents()
+        val numberOfItemsInDb = dao.numberOfOsAscentsByClimbingType(ClimbingType.SPORT).getOrAwaitValue()
+        val numOfSportAscents = ascents.count { it.climbingType == ClimbingType.SPORT }
+        assertEquals(numberOfItemsInDb,numOfSportAscents)
+    }
+
+    @Test
+    fun getLastAscents_lastThree() = runTest {
+        insertAscents()
+        val ascentsInDb = dao.getLastAscents(3).getOrAwaitValue()
+        val lastThreeAscents = ascents.sortedByDescending { it.date }.take(3)
+        assertEquals(ascentsInDb,lastThreeAscents)
+    }
+
+    @Test
+    fun getLastAscents_empty() = runTest {
+        insertAscents()
+        val ascentsInDb = dao.getLastAscents(0).getOrAwaitValue()
+        assertEquals(ascentsInDb, listOf<Ascent>())
+    }
+
+    @Test
+    fun getLastAscents_minusOne() = runTest {
+        insertAscents()
+        val ascentsInDb = dao.getLastAscents(-1).getOrAwaitValue()
+        assertEquals(ascentsInDb, ascents.sortedByDescending { it.date })
+    }
+
+    @Test
+    fun getLastAscents_overSize() = runTest {
+        insertAscents()
+        val ascentsInDb = dao.getLastAscents(31).getOrAwaitValue()
+        assertEquals(ascentsInDb, ascents.sortedByDescending { it.date })
+    }
+
+
+
 }
