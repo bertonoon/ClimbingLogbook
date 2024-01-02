c445d858612dd3a53f7e0170ddb5b0eca3a2d94f
bart <filipow.bartosz@gmail.com>
2024-01-02 18:27:18 +0100

    add example barChart

--- app/src/main/java/com/bf/climbinglogbook/ui/statistics/StatisticsFragment.kt
+++ app/src/main/java/com/bf/climbinglogbook/ui/statistics/StatisticsFragment.kt
@@ -4,37 +4,68 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.viewModels
+import com.anychart.AnyChart
+import com.anychart.chart.common.dataentry.DataEntry
+import com.anychart.chart.common.dataentry.ValueDataEntry
+import com.anychart.chart.common.listener.Event
+import com.anychart.chart.common.listener.ListenersInterface
+import com.anychart.enums.Align
+import com.anychart.enums.LegendLayout
+import com.bf.climbinglogbook.R
 import com.bf.climbinglogbook.databinding.FragmentStatisticsBinding
+import com.bf.climbinglogbook.ui.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class StatisticsFragment : Fragment() {
 
     private var _binding: FragmentStatisticsBinding? = null
-
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+    private val mainViewModel: MainViewModel by activityViewModels()
+    private val statisticsViewModel: StatisticsViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val dashboardViewModel =
-            ViewModelProvider(this).get(StatisticsViewModel::class.java)
-
         _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        binding.toolbar.title.text = getString(R.string.title_statistics)
+        //initListeners()
+        setupBarChart()
+        return binding.root
+    }
 
-        val textView: TextView = binding.textDashboard
-        dashboardViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+    private fun initListeners() {
+
+    }
+
+    private fun setupBarChart() {
+        val pieChart = AnyChart.pie()
+
+        val data: MutableList<DataEntry> = mutableListOf()
+        data.add(ValueDataEntry("Apples", 6371664))
+        data.add(ValueDataEntry("Pears", 789622))
+        data.add(ValueDataEntry("Bananas", 7216301))
+        data.add(ValueDataEntry("Grapes", 1486621))
+        data.add(ValueDataEntry("Oranges", 1200000))
+
+        pieChart.data(data)
+        pieChart.title("Test")
+        pieChart.labels().position("outside")
+        pieChart.legend().enabled(true)
+        pieChart.legend().title().text("legend").padding(0.0, 0.0, 10.0, 0.0)
+        pieChart.legend().position("center-bottom").itemsLayout(LegendLayout.HORIZONTAL)
+            .align(Align.CENTER)
+
+        binding.anyChartView.setChart(pieChart)
+
     }
 
     override fun onDestroyView() {
